a5c4ec214aee18559eda319194bb9ee5b78e94ff
Scott Shetley <davidscottshetley@gmail.com>
2025-02-22 23:12:10 -0500

    removed button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.scrollTo(0, 0); // Scroll to the top of the page on load
 
-    // Example button click event
-    document.getElementById('myButton').addEventListener('click', function() {
-        alert('Button was clicked!');
-    });
-
     // Initialize Fancybox for the design gallery
     $('[data-fancybox="gallery"]').fancybox();
     $('[data-fancybox="design-gallery"]').fancybox(); // Add this line to initialize Fancybox for the design gallery
